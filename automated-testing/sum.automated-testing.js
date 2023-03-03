c0567c6f011f66cc67b15a1bc6abed3e3695bbf8
Zikang CHEN <appointat@shu.edu.cn>
2023-03-03 23:52:53 +0100

    Update automated testing of sum function

--- automated-testing/sum.automated-testing.js
+++ automated-testing/sum.automated-testing.js
@@ -1,9 +1,15 @@
 // import sum from '../front-end/sum.js'
-const sum = require('../front-end/sum.js')
+const sum = require('../front-end/sum')
 
 test(
-    'adds 1 + 2 to equal 3',
-    () => {
+    'adds 1 + 2 to equal 3', () => {
+        console.log(`result = ${sum(1, 2)}\n`);
         expect(sum(1, 2)).toBe(3);
-    });
+});
+
+test('object assignment', () => {
+    const data = { one: 1 };
+    data['two'] = 2;
+    expect(data).toEqual({ one: 1, two: 2 });
+});
 
